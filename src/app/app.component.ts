a17939131e9fdac0a7596329cde2ce90f10ff288
Piotr Fraszczak <piotr.fraszczak@hotmail.com>
2024-06-08 07:58:25 +0200

    new 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostBinding } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostBinding, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header';
 import { FooterComponent } from './components/footer';
 
@@ -19,4 +19,14 @@ import { FooterComponent } from './components/footer';
 })
 export class AppComponent {
   @HostBinding('class') hostClass = 'flex flex-col w-full h-full';
+
+  #router = inject(Router);
+
+  constructor() {
+    let path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.#router.navigate([path]);
+    }
+  }
 }
