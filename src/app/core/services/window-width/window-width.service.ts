d397e477e539c198620e7b0cb2926b71157c5ab8
Piotr Fraszczak <piotr.fraszczak@hotmail.com>
2024-06-22 17:03:35 +0200

    width service

---
+++ src/app/core/services/window-width/window-width.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, computed, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WindowWidthService {
+  private width = signal(window.innerWidth);
+
+  constructor() {
+    window.addEventListener('resize', () => {
+      this.width.set(window.innerWidth);
+    });
+  }
+
+  get isMobile() {
+    return computed(() => this.width() <= 768);
+  }
+}
